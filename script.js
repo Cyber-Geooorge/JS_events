5769316fadab1db71e08b77c8cec7f4a93cc9de6
Cyber-Geooorge <85462340+Cyber-Geooorge@users.noreply.github.com>
2023-03-07 07:45:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
 // Fonctionnalité 1 + 1-bis :
 
+let footer = document.querySelector("footer");
+
+let nbrClicks = 0;
+
+footer.addEventListener("click", function() {
+  
+    nbrClicks += 1;
+
+    console.log("Clic numéro ${nbrClicks}");
+  
+});
+
+
 // Fonctionnalité 2 :
+
+const togg = () => {
+    navbarHeader.classList.toggle('collapse');
+}
+const menu = document.querySelector('button').addEventListener('click', togg);
+
+
 
 // Fonctionnalité 3 :
 
+document.getElementsByClassName("btn-outline-secondary")[0].addEventListener('click', () => {
+  
+    document.getElementsByClassName("card-text")[0].style.color = 'red'
+  
+});
+  
+
 // Fonctionnalité 4 :
 
+function toggGreen() {
+  
+    let styleCard = document.querySelector('.col-md-4:nth-child(2) .card-text')
+    
+    document.querySelector('.col-md-4:nth-child(2) .btn-outline-secondary').addEventListener("click", function () {
+
+      if (styleCard.style.color === 'green') {
+
+        styleCard.style.color = '';
+
+      } else {
+        styleCard.style.color = "green";
+      }
+
+    })
+
+};
+
+
 // Fonctionnalité 5 :
+
+const targLink = document.getElementsByTagName('link')[0];
+
+const navbar = document.querySelector('header');
+
+navbar.addEventListener('dblclick', () => {
+    targLink.disabled === false ? targLink.disabled = true : targLink.disabled = false;
+});
+
 
 // Fonctionnalité 6 :
 
